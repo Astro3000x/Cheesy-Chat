1711c840d992a186af6a44246317b50737dd092e
Astro3000 <94796723+Astro3000x@users.noreply.github.com>
2022-04-15 00:26:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cheesy #
+**The Chatroom About Cheese**
+
+**Try It [Here](https://cheesychat.astro3000.repl.co)**
+
+Developers: [Astro3000](https://astrodev.astro3000.repl.co),
+[Chickenman0426](https://chickenmans-stuff.chickenman0426.repl.co)
+## Some Code Sourced From: ##
+[ReplIt](https://www.youtube.com/c/replityt)
---
+++ html/script.js
@@ -0,0 +1,104 @@
+var devs = ["Astro 3000", "chickenman thegod"];
+var shareholder = ["Boy Bearcat"];
+
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  
+  document.getElementById("profileimg").src = profile.getImageUrl();
+  
+  document.getElementById("profilename").innerHTML = profile.getName();
+  document.getElementById("NameInput").value = profile.getName();
+  
+  
+  if (devs.includes(profile.getName())) {
+    document.getElementById("devm").innerHTML = "Developer Team";
+  }
+  if (shareholder.includes(profile.getName())) {
+    document.getElementById("devm").innerHTML = "Share Holder Guy";
+  }
+  
+  
+}
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    });
+  document.getElementById("profileimg").src = "";
+  
+  document.getElementById("profilename").innerHTML = "";
+  
+  
+
+  document.getElementById("devm").innerHTML = ""
+  document.getElementById("NameInput").value = "";
+}
+
+
+
+
+
+
+
+
+
+
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var dingSound;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Server: " + room;
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+      
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#ECDC68;";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
